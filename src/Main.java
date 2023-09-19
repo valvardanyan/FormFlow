465881ad888a29ff62c5d5a7299b7123e17d3df6
Valeriya Vardanyan <tun28054@temple.edu>
2023-09-18 22:00:43 -0400

    Program takes a pdf, writes on it, and outputs it

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,36 @@
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.PDPageTree;
+import org.apache.pdfbox.pdmodel.font.PDType0Font;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        try {
+            PDDocument document1 = PDDocument.load(new File("IncompleteFiles/Blank.pdf"));
+            PDPageTree pages1 = document1.getPages();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+            PDPage page1 = pages1.get(0);
+            PDType0Font font = PDType0Font.load(document1, new File("libraries/Roboto-Regular.ttf"));
+            PDPageContentStream contentStream1 = new PDPageContentStream(document1, page1);
+            contentStream1.setFont(font, 12);
+            contentStream1.beginText();
+            contentStream1.newLineAtOffset(50, page1.getMediaBox().getHeight() - 50);
+            contentStream1.showText("Hello World");
+            contentStream1.endText();
+            contentStream1.close();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            document1.save(new File("CompletedFiles/WrittenOn.pdf"));
+            document1.close();
+
+            System.out.println("Printed on PDF");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
